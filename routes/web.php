4a0cbf020cd5d2db02bc78741f27b0e87783f47f
Junior Lima <juniorlima.dev@gmail.com>
2024-10-13 18:50:55 -0300

    build: Instalar pacote do adminlte3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
-Route::get('/', function () {
+Route::get('/', function (): View {
+    return view('home');
+});
+
+Route::get('/welcome', function (): View {
     return view('welcome');
 });
