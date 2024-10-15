5e4439e5d9e35977858a2efb67f9266e93837f77
Junior Lima <juniorlima.dev@gmail.com>
2024-10-15 03:03:35 -0300

    feat(Cliente): Criar componente e formulário cadastrar Cliente

--- app/Livewire/Cliente/ClienteCreate.php
+++ app/Livewire/Cliente/ClienteCreate.php
@@ -2,11 +2,50 @@
 
 namespace App\Livewire\Cliente;
 
+use App\Http\Requests\ClienteRequest;
+use App\Service\ClienteService;
+use App\Traits\Toast;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class ClienteCreate extends Component
 {
+    use Toast;
+    protected ClienteService $clienteService;
+
+    public string $nome;
+    public string $cpf;
+    public string $telefone;
+    public string $email;
+    public string $cep;
+    public string $logradouro;
+    public string $numero;
+    public string $cidade;
+    public string $bairro;
+    public string $complemento;
+
+    public function boot(ClienteService $clienteService)
+    {
+        $this->clienteService = $clienteService;
+    }
+
+    public function rules(): array
+    {
+        return (new ClienteRequest())->rules();
+    }
+
+    public function store(): void
+    {
+        $cliente = $this->clienteService->cadastrar($this->validate());
+
+        if ($cliente != null) {
+            $this->sucesso($this->registro_cadastrado);
+            $this->reset();
+        } else {
+            $this->error($this->registro_erro_cadastrar);
+        }
+    }
+
     public function render(): View
     {
         return view('livewire.cliente.cliente-create');
--- resources/views/livewire/cliente/cliente-create.blade.php
+++ resources/views/livewire/cliente/cliente-create.blade.php
@@ -1,3 +1,84 @@
 <div>
-    The whole world belongs to you. 
+    <x-adminlte-card class="shadow">
+        <form wire:submit.prevent="store">
+            <div class="form-row">
+                <div class="col-md-6">
+                    <x-adminlte-input type="text" wire:model="nome" name="nome" label="Nome" placeholder="Nome do Cliente" enable-old-support />
+                </div>
+
+                <div class="col-md-6">
+                    <x-adminlte-input type="text" wire:model="cpf" name="cpf" label="CPF" placeholder="CPF" enable-old-support autocomplete="off" />
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="col-md-6">
+                    <x-adminlte-input type="phone" wire:model="telefone" name="telefone" label="Telefone" placeholder="Telefone" enable-old-support>
+                        <x-slot name="appendSlot">
+                            <div class="input-group-text text-dark">
+                                <i class="fas fa-phone-alt"></i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
+                </div>
+
+                <div class="col-md-6">
+                    <x-adminlte-input type="text" wire:model="email" name="email" label="E-mail" placeholder="E-mail" enable-old-support autocomplete="off">
+                        <x-slot name="appendSlot">
+                            <div class="input-group-text text-dark">
+                                <i class="fas fa-at"></i>
+                            </div>
+                        </x-slot>
+                    </x-adminlte-input>
+                </div>
+            </div>
+
+            <hr>
+            <h4>Endereço</h4>
+
+            <div class="form-row">
+                <div class="col-md-4">
+                    <x-adminlte-input type="text" wire:model="cep" name="cep" label="CEP" placeholder="CEP" enable-old-support />
+                </div>
+
+                <div class="col-md-4">
+                    <x-adminlte-input type="text" wire:model="logradouro" name="logradouro" label="Rua" placeholder="Rua" enable-old-support />
+                </div>
+
+                <div class="col-md-4">
+                    <x-adminlte-input type="text" wire:model="numero" name="numero" label="Número" placeholder="Número" enable-old-support />
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="col-md-4">
+                    <x-adminlte-input type="text" wire:model="cidade" name="cidade" label="Cidade" placeholder="Cidade" enable-old-support />
+                </div>
+
+                <div class="col-md-4">
+                    <x-adminlte-input type="text" wire:model="bairro" name="bairro" label="Bairro" placeholder="Bairro" enable-old-support />
+                </div>
+
+                <div class="col-md-4">
+                    <x-adminlte-input type="text" wire:model="complemento" name="complemento" label="Complemento" placeholder="Complemento" enable-old-support />
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-success mr-1">
+                <span wire:loading wire:target="store">
+                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    <span class="sr-only">Carregando...</span>
+                </span>
+                <i class="fas fa-save" wire:loading.remove wire:target="store"></i>
+                Salvar
+            </button>
+
+            <x-adminlte-button label="Voltar" icon="fas fa-arrow-circle-left" onclick="window.history.back()" />
+        </form>
+    </x-adminlte-card>
+
+    @section('js')
+    <script>
+    </script>
+    @stop
 </div>
