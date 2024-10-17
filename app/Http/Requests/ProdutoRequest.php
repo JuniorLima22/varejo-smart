84c6c4f04cf4bffaa1208a6fabbeb5f5a1763856
Junior Lima <juniorlima.dev@gmail.com>
2024-10-17 02:43:17 -0300

    feat(Produto): Criar validação dos dados ao cadastrar ou atualizar Produto

---
+++ app/Http/Requests/ProdutoRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProdutoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(int $produtoId = 0): array
+    {
+        $rules = [
+            'nome' => ['required', 'string', 'min:3', 'max:255', "unique:produtos,nome,{$produtoId},id"],
+            'descricao' => ['nullable', 'string', 'min:3', 'max:65535'],
+            'preco_compra' => ['required', 'min:0', 'decimal:0,2'],
+            'preco_venda' => ['required', 'min:0', 'decimal:0,2'],
+            'categoria_id' => ['required', 'exists:categorias,id'],
+            'quantidade' => ['required', 'integer', 'min:0'],
+            'imagem_url' => ['nullable', 'string', 'min:3', 'max:255'],
+        ];
+
+        return $rules;
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'categoria_id'=> 'categoria',
+            'imagem_url'=> 'imagem',
+        ];
+    }
+}
