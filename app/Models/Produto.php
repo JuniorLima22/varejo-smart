d54db9bc3468c21d85086ad1e44dac45d868e4f3
Junior Lima <juniorlima.dev@gmail.com>
2024-10-19 00:17:19 -0300

    feat(Produto): Adicionar relacionamentos com ItensVenda

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Produto extends Model
 {
@@ -23,5 +24,10 @@ class Produto extends Model
     public function categoria(): BelongsTo
     {
         return $this->belongsTo(Categoria::class);
+    }
+
+    public function itensVenda(): HasMany
+    {
+        return $this->hasMany(ItensVenda::class);
     }
 }
