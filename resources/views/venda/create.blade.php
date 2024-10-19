06bb1a27c48fb1e0e24bffa1818c4da3e446c407
Junior Lima <juniorlima.dev@gmail.com>
2024-10-19 05:43:30 -0300

    feat(Venda): Adicionar data atual no cabeçalho da pagina

--- resources/views/venda/create.blade.php
+++ resources/views/venda/create.blade.php
@@ -3,7 +3,14 @@
 @section('title', 'Cadastrar Venda')
 
 @section('content_header')
-    <h1>Cadastrar Venda</h1>
+    <div class="row">
+        <div class="col-6 float-left text-left">
+            <h1>Cadastrar Venda</h1>
+        </div>
+        <div class="col-6 float-right text-right">
+            <h6 class="font-weight-bold">Hoje, {{ ucfirst(now()->translatedFormat('l')) }} {{ now()->format('d/m/Y') }}</h6>
+        </div>
+    </div>
 @stop
 
 @section('content')
--- resources/views/venda/edit.blade.php
+++ resources/views/venda/edit.blade.php
@@ -4,7 +4,14 @@
 @section('title', 'Editar Venda')
 
 @section('content_header')
-    <h1>Editar Venda</h1>
+    <div class="row">
+        <div class="col-6 float-left text-left">
+            <h1>Editar Venda</h1>
+        </div>
+        <div class="col-6 float-right text-right">
+            <h6 class="font-weight-bold">Hoje, {{ ucfirst(now()->translatedFormat('l')) }} {{ now()->format('d/m/Y') }}</h6>
+        </div>
+    </div>
 @stop
 
 @section('content')
